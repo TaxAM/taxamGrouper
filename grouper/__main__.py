99fd3d539cd1d78bfdaa5a6b3d88e110d1af90da
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-04-30 18:30:55 -0300

    kmeans function in grouper_algorithms module pt1

--- grouper/__main__.py
+++ grouper/__main__.py
@@ -6,17 +6,16 @@ algorithms = {
 }
 
 matrix = pd.read_csv(
-    './src/saida-reino-no-one.taxam',
+    'grouper/src/saida-reino-no-one.taxam',
     delimiter = '\t',
     index_col= 'TaxAM'
 )
-
 
 print('Original Matrix:')
 print(matrix)
 
 transposed_matrix = matrix.transpose()
-groups = algorithms['kmeans'](matrix, 2)
+groups = algorithms['kmeans'](transposed_matrix, 2)
 
 print('\nNumber of groups: 2.')
 print('Groups:')
