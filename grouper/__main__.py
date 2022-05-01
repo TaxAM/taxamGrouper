f1348f9ac21e9f05cb736e96f107f628f7359845
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-04-30 23:00:46 -0300

    introduce functools

--- grouper/__main__.py
+++ grouper/__main__.py
@@ -1,22 +1,31 @@
 import argparse
+import functools
 from modules.grouper_algorithms import kmeans
 import pandas as pd
 
-algorithms = {
-    'kmeans': kmeans,
-    'somenthing': 'somenthing',
-    'somenthing1': 'somenthing',
-
-}
-
 parse = argparse.ArgumentParser(description='Executes taxam grouper.', usage='python ./grouper/ -a "kmeans"')
+
+algorithms = {
+    'kmeans': {
+        'name': kmeans,
+        'flags': [
+            functools.partial(parse.add_argument, '-n', '--n_clusters', help = 'Set the number of clusters that Kmeans will use to group samples.',type = int, action = 'store', default = 2)
+        ]
+    }
+}
 
 parse.add_argument('-a', '--algorithm', help = 'Choise which algorithm taxam grouper will use to grouper samples.',type = str, action = 'store', default = 'kmeans', choices = algorithms.keys())
 
-args = parse.parse_args().__dict__
 # Chosen algorithm by user to grouper samples
-op_algorithm = args['algorithm']
+op_algorithm = parse.parse_args().__dict__['algorithm']
 
+# Storing flags for algorithm chosen in parse
+
+
+args = parse.parse_args().__dict__
+
+print(algorithms)
+exit('Stoped')
 matrix = pd.read_csv(
     'grouper/src/saida-reino-no-one.taxam',
     delimiter = '\t',
--- grouper/modules/grouper_algorithms/__init__.py
+++ grouper/modules/grouper_algorithms/__init__.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import argparse
 def kmeans(matrix, n_clusters):
     '''
     kmeans algorithm
     '''
+
+    parse = argparse.ArgumentParser(description='Kmeans algorithm', usage='-n 2')
+
+    parse.add_argument('-n', '--n_clusters', help = 'Set the number of clusters that Kmeans will use to group samples.',type = int, action = 'store', default = 2)
+
     from sklearn.cluster import KMeans
 
     print('\nTransposed Matrix:')
