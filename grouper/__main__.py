a753adec809edf8082ba968c9805d1993e71f0d5
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-05-02 16:01:59 -0300

    veryfing valid taxam file as input

--- grouper/__main__.py
+++ grouper/__main__.py
@@ -1,8 +1,10 @@
 import argparse
-import sys
 import functools
+from posixpath import split
 from modules.grouper_algorithms import kmeans
+from os.path import exists
 import pandas as pd
+import sys
 
 # Chosen algorithm by user to grouper samples
 op_algorithm = sys.argv[1] if len(sys.argv) > 1 else 'kmeans'
@@ -15,10 +17,27 @@ algorithms = {
     'kmeans': {
         'function': kmeans,
         'flags': [
-            functools.partial(parse.add_argument, '-n', '--n_clusters', help = 'Set the number of clusters that Kmeans will use to group samples.',type = int, action = 'store', default = 2)
+            functools.partial(
+                parse.add_argument,
+                '-n',
+                '--n_clusters',
+                help = 'Set the number of clusters that Kmeans will use to group samples.',
+                type = int,
+                action = 'store',
+                default = 2
+            )
         ]
     }
 }
+# Default flags
+parse.add_argument(
+    '-fp',
+    '--file_path',
+    help = 'Matrix to group.',
+    type = str,
+    action = 'store',
+    default = None
+)
 
 # Check if op_algorithm is a valid algorithm
 if op_algorithm not in algorithms_list:
@@ -31,8 +50,18 @@ for flag in algorithms[op_algorithm]['flags']:
 # Arguments to the algorithms functions
 args = parse.parse_args().__dict__
 
+# Check if the file is valid
+if args['file_path'] != None and\
+     args['file_path'].split('.')[-1].lower() == 'taxam' :
+    # Check if matrix exists
+    if not exists(args['file_path']):
+        exit(args['file_path'] + ' does not exists.')
+else:
+    exit('You should inform a matrix valid file.')
+
+
 matrix = pd.read_csv(
-    'grouper/src/saida-reino-no-one.taxam',
+    args['file_path'],
     delimiter = '\t',
     index_col= 'TaxAM'
 )
